8bb900a75f609ef83c1e77ed8cea11490f3f2e6b
Siddharth Basale <siddharthbasale2004@gmail.com>
2026-03-20 15:56:46 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from app import add
+from ap import add
 
 def test_add():
-    add(2, 2)
+    add(2, 2
